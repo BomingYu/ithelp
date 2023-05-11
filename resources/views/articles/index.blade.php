45e2ea8ca96f6e48ba9ffb4fa9d74d87044f02b1
by39 <by39@students.waikato.ac.nz>
2023-05-12 01:33:23 +1200

    article functions added

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts/article')
+
+@section('main')
+   <h1 class="font-thin text-4x1">Articles List</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,12 @@
 <?php
 
+use App\Http\Controllers\articleController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('articles', articleController::class);
+
+Route::get('/',[articleController::class,'index'])->name('root');
 
 Route::middleware([
     'auth:sanctum',
